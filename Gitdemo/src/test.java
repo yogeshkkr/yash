e78768a2987bf97238de21875683068679d2a14a
yogeshkkr <preethi.kumari2020@gmail.com>
2017-01-30 23:17:08 +0530

    firstcodeupdate

--- Gitdemo/src/test.java
+++ Gitdemo/src/test.java
@@ -3,6 +3,10 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println( "changeses1");
+		
+		System.out.println( "changeses2");
+		
+		System.out.println( "changeses3");
 	}
 
 }
